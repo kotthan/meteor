1383723dc832269c27cc3050500b33961ce644e2
Tamiwo <taimwo.otutama@gmail.com>
2018-02-11 00:36:52 +0900

    下スワイプ継続中にguardActionを呼ぶように

--- MeteorSample_20170831/SKGameSample/GameScene.swift
+++ MeteorSample_20170831/SKGameSample/GameScene.swift
@@ -553,8 +553,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             return
         }
         
-        /*for touch: AnyObject in touches
+        for touch: AnyObject in touches
         {
+            let endPosOnView = CGPoint(x: touch.location(in: view).x,
+                                       y: frame.maxY - touch.location(in: view).y )
+            drawTouchPath(begin: beganPosOnView, end: endPosOnView)
+            switch getTouchAction(begin: beganPosOnView, end: endPosOnView) {
+            case .tap:
+                break   //何もしない
+            case .swipeDown:
+                guardAction(endFlg: false)
+            case .swipeUp: //ジャンプしてない場合のみ
+                break   //何もしない
+            case .swipeLeft: //ジャンプしてない場合のみ
+                break   //何もしない
+            case .swipeRight://ジャンプしてない場合のみ
+                break   //何もしない
+            }
+            /*
             let endedPos = touch.location(in: self)                          //タップを話した点を定義
             let cameraMoveY = ( (camera?.position.y)! -  self.beganPyPos )   //前回からのカメラの移動量を求める
             self.beganPyPos = (camera?.position.y)!                          //次回計算時のために現在位置を覚える
@@ -595,7 +611,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             {
                 baseNode.addChild(touchPath)
             }
-         }*/
+            */
+         }
     }
     
     //MARK: タッチアップされたときに呼ばれる関数
